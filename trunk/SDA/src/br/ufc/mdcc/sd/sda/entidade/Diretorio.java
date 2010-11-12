783d6a202eb1a94f8a2993cbcf700c452a990985
fabiano.bie <fabiano.bie@13df6e1f-61dc-5749-511e-35982fc7f6c3>
2010-11-12 23:41:31 +0000

    Fabiano

--- trunk/SDA/src/br/ufc/mdcc/sd/sda/entidade/Diretorio.java
+++ trunk/SDA/src/br/ufc/mdcc/sd/sda/entidade/Diretorio.java
@@ -1,5 +1,26 @@
 package br.ufc.mdcc.sd.sda.entidade;
 
-public class Diretorio {
+import java.util.HashMap;
+
+public class Diretorio extends Arquivo{
+	
+	private HashMap<String, Ufid> mapeamento;
+	
+	/**
+	 * 
+	 */
+	public Diretorio() {
+		
+		mapeamento = new HashMap<String, Ufid>();
+		getDescritor().setTipo(TipoArquivo.DIRECTORY);
+		
+	}
+	
+	public HashMap<String, Ufid> getMapeamento() {
+		return mapeamento;
+	}
+	public void setMapeamento(HashMap<String, Ufid> mapeamento) {
+		this.mapeamento = mapeamento;
+	}
 
 }
