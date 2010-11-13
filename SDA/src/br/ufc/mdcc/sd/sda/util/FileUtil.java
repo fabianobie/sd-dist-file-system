0522e2b795c13b186d005bd00557ae869702136d
fabiano.bie <fabiano.bie@13df6e1f-61dc-5749-511e-35982fc7f6c3>
2010-11-13 13:24:00 +0000

    Fabiano

--- SDA/src/br/ufc/mdcc/sd/sda/util/FileUtil.java
+++ SDA/src/br/ufc/mdcc/sd/sda/util/FileUtil.java
@@ -1,5 +1,38 @@
 package br.ufc.mdcc.sd.sda.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+
 public class FileUtil {
+
+	public static void serializarObjeto(Object object, String nome) {
+
+		try {
+			ObjectOutput out = new ObjectOutputStream(new FileOutputStream(nome));
+			out.writeObject(object);
+			out.close();
+
+		} catch (IOException e) {
+		}
+
+	}
+
+	public static Object deserializarObjeto(String nome) {
+		Object object = null;
+		try {
+			File file = new File(nome);
+			ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+			object = in.readObject();
+			in.close();
+		} catch (ClassNotFoundException e) {
+		} catch (IOException e) {
+		}
+		return object;
+	}
 
 }
