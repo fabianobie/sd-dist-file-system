8151715fae4a6e451e892d8edbb47b1da9a9dbd1
fabiano.bie <fabiano.bie@13df6e1f-61dc-5749-511e-35982fc7f6c3>
2010-11-13 13:23:24 +0000

    Fabiano

---
+++ trunk/SDA/src/br/ufc/mdcc/sd/sda/entidade/Permissao.java
@@ -0,0 +1,82 @@
+package br.ufc.mdcc.sd.sda.entidade;
+
+public class Permissao {
+	/**
+	 * Id do Usuario ou grupo que possui permissoes
+	 */
+	private int idEntidade;
+	
+	/**
+	 * Permissões
+	 */
+	private boolean read;
+	private boolean write;
+	private boolean delete;
+	private boolean getAtt;
+	private boolean setAtt;
+	
+	/**
+	 * @param idEntidade
+	 * @param read
+	 * @param write
+	 * @param delete
+	 * @param getAtt
+	 * @param setAtt
+	 */
+	public Permissao(int idEntidade, boolean read, boolean write,
+			boolean delete, boolean getAtt, boolean setAtt) {
+		super();
+		
+		this.idEntidade = idEntidade;
+		this.read = read;
+		this.write = write;
+		this.delete = delete;
+		this.getAtt = getAtt;
+		this.setAtt = setAtt;
+	}
+
+	public int getIdEntidade() {
+		return idEntidade;
+	}
+
+	public boolean isRead() {
+		return read;
+	}
+
+	public void setRead(boolean read) {
+		this.read = read;
+	}
+
+	public boolean isWrite() {
+		return write;
+	}
+
+	public void setWrite(boolean write) {
+		this.write = write;
+	}
+
+	public boolean isDelete() {
+		return delete;
+	}
+
+	public void setDelete(boolean delete) {
+		this.delete = delete;
+	}
+
+	public boolean isGetAtt() {
+		return getAtt;
+	}
+
+	public void setGetAtt(boolean getAtt) {
+		this.getAtt = getAtt;
+	}
+
+	public boolean isSetAtt() {
+		return setAtt;
+	}
+
+	public void setSetAtt(boolean setAtt) {
+		this.setAtt = setAtt;
+	}
+
+}
